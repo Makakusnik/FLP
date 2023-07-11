fb366c1ebd901ef05c9cab91830dd1c5f1933857
Marek Fodor <fodormarek5@gmail.com>
2023-07-11 12:20:09 +0200

    Add Widget container

--- src/lib/common/widgets/WidgetContainer.svelte
+++ src/lib/common/widgets/WidgetContainer.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-	import Icon from '@iconify/svelte';
 	export let className: string = '';
+	export let title: string = '';
+	let showTopList = $$slots.dropdown;
 </script>
 
 <section class="widget {className}">
-	<div class="list">
-		<button class="button">
-			<Icon icon="ri:refresh-line" class="w-5 h-5" />
-		</button>
-		<button class="button">
-			<Icon icon="pepicons-pop:dots-y" class="w-5 h-5" />
-		</button>
-	</div>
-	<div class="content">
-		<slot />
-	</div>
+	{#if showTopList}
+		<div class="list">
+			<p class="title">
+				{title}
+			</p>
+			<div class="dropdown">
+				<slot name="dropdown" />
+			</div>
+		</div>
+	{/if}
+	<slot />
 </section>
 
 <style lang="postcss">
+	.title {
+		@apply font-medium text-slate-200 text-sm;
+	}
+
+	.dropdown {
+		@apply ml-auto;
+	}
+
 	.widget {
-		@apply flex flex-col bg-slate-700 w-full h-full min-h-[16rem];
+		@apply flex flex-col bg-slate-700 w-full h-[16rem] overflow-hidden;
 		@apply p-3;
 	}
 
 	.list {
-		@apply flex w-full h-fit justify-between;
-	}
-
-	.list > .button {
-		@apply p-1 rounded-sm transition-colors;
-		@apply hover:bg-slate-100/20;
-	}
-
-	.content {
-		@apply pt-2;
+		@apply flex w-full h-fit justify-between mb-2;
 	}
 </style>
