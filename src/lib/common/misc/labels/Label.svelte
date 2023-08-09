7ca54f254d145d602d9bc5e9f7b1aaf6d02548ac
Marek Fodor <fodormarek5@gmail.com>
2023-08-09 15:25:46 +0200

    feat(assets): add collapsible small label

---
+++ src/lib/common/misc/labels/Label.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let text: string;
+	export let color: string;
+	export let textColor: string;
+	export let collapsed = true;
+</script>
+
+<button
+	class="label"
+	style="background-color: {color}; color: {textColor}"
+	class:collapsed
+	on:click>
+	<p>{text}</p>
+</button>
+
+<style lang="postcss">
+	.label {
+		@apply flex items-center rounded-sm h-4 min-w-[2rem] w-fit overflow-hidden text-xs font-semibold px-2;
+		transition: height 200ms ease-in-out;
+	}
+	p {
+		transition: opacity 100ms 100ms ease-in-out;
+	}
+	.collapsed {
+		@apply h-2;
+	}
+
+	.collapsed > p {
+		transition: opacity 50ms ease-in-out;
+		opacity: 0;
+		pointer-events: none;
+	}
+</style>
