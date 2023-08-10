e7581021483b7830c8f2cf87091980b3c58832f3
Marek Fodor <fodormarek5@gmail.com>
2023-08-10 14:57:20 +0200

    feat(assets): add new assets, update readme

---
+++ src/lib/assets/icons/ShowFillIcon.svelte
@@ -0,0 +1,9 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	width="24"
+	height="24"
+	fill="currentcolor"
+	class={$$props.class}
+	><path
+		d="M12 9a3 3 0 1 0 0 6 3 3 0 1 0 0-6m0 8a5 5 0 1 1 0-10 5 5 0 1 1 0 10m0-12.5C7 4.5 2.73 7.609 1 12c1.73 4.391 6 7.5 11 7.5s9.27-3.109 11-7.5c-1.73-4.391-6-7.5-11-7.5zm0 0" /></svg>
