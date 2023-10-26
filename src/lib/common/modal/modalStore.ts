6f5ab7a3aa42ce92d6c0e63c9686d061bb07c82f
Marek Fodor <fodormarek5@gmail.com>
2023-10-26 13:04:39 +0200

    feature: add options

--- src/lib/common/modal/modalStore.ts
+++ src/lib/common/modal/modalStore.ts
@@ -1,18 +1,57 @@
 import { writable } from 'svelte/store';
 
+export type ModalStateOptions = {
+	defaultValue?: boolean;
+	storeName?: string;
+	openCallback?: () => void;
+	closeCallback?: () => void;
+};
+
 // Toto bude fungovat iba v pripade, ze sa mi podari fixnut bug kde escapom zatvorim hocjaky modal, aj ten, ktory je pod najvyssie zobrazenym modalom
-export function useModalState(storeName?: string) {
-	// Just for debug purposes.. will delete soon, hopefuly
-	const startStop = () => {
-		console.log('Got first sub: ', storeName);
-		return () => {
-			console.log('No more subs: ', storeName);
+export function useModalState(arg1?: string | ModalStateOptions) {
+	if (typeof arg1 === 'string') {
+		const startStop = () => {
+			console.log('Got first sub: ', arg1);
+			return () => {
+				console.log('No more subs: ', arg1);
+			};
 		};
-	};
-	const { set, subscribe, update } = writable(false, storeName ? startStop : undefined);
+		const { set, subscribe, update } = writable(false, arg1 ? startStop : undefined);
 
-	const open = () => update(() => true);
-	const close = () => update(() => false);
+		const open = () => update(() => true);
+		const close = () => update(() => false);
 
-	return { isOpened: { subscribe, set }, open, close };
+		return { isOpened: { subscribe, set }, open, close };
+	} else if (typeof arg1 === 'object') {
+		const storeName = arg1?.storeName ? arg1.storeName : '';
+		const startStop = () => {
+			console.log('Got first sub: ', storeName);
+			return () => {
+				console.log('No more subs: ', storeName);
+			};
+		};
+		const { set, subscribe, update } = writable(
+			Boolean(arg1?.defaultValue),
+			storeName ? startStop : undefined
+		);
+
+		const open = () =>
+			update(() => {
+				arg1?.openCallback && arg1.openCallback();
+				return true;
+			});
+		const close = () =>
+			update(() => {
+				arg1?.closeCallback && arg1?.closeCallback();
+				return false;
+			});
+
+		return { isOpened: { subscribe, set }, open, close };
+	} else {
+		const { set, subscribe, update } = writable(false);
+		const open = () => update(() => true);
+		const close = () => update(() => false);
+
+		return { isOpened: { subscribe, set }, open, close };
+	}
 }
